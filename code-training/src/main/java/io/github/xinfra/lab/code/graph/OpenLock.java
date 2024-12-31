9f92782a5e8ccb8feb3566fd433de812e63809db
joecqupt <joe469391363@gmail.com>
2025-01-01 04:46:35 +0800

    update

--- code-training/src/main/java/io/github/xinfra/lab/code/graph/OpenLock.java
+++ code-training/src/main/java/io/github/xinfra/lab/code/graph/OpenLock.java
@@ -1,7 +1,7 @@
 package io.github.xinfra.lab.code.graph;
 
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -14,42 +14,45 @@ public class OpenLock {
 
     // https://leetcode.cn/problems/open-the-lock/
 
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        int i = solution.openLock(new String[]{
-                "8887","8889","8878","8898","8788","8988","7888","9888"
-        }, "8888");
-        System.out.println(i);
-    }
-
-    static class Solution {
+     class Solution {
         public int openLock(String[] deadends, String target) {
+            String start = "0000";
+            Set<String> deadendSet = new HashSet<>();
+            for (String deadend : deadends){
+                deadendSet.add(deadend);
+            }
+            if (deadendSet.contains(start)) {
+                return -1;
+            }
             Queue<String> queue = new LinkedList<>();
             Set<String> visited = new HashSet<>();
-            Set<String> deadendSet = Arrays.stream(deadends).collect(Collectors.toSet());
-            queue.add("0000");
+            queue.offer(start);
             int step = 0;
             while (!queue.isEmpty()) {
                 int size = queue.size();
+                List<String> list = new ArrayList<>();
                 for (int i = 0; i < size; i++) {
                     String value = queue.poll();
                     if (Objects.equals(value, target)) {
                         return step;
                     }
                     visited.add(value);
-                    if (deadendSet.contains(value)) {
-                        continue;
-                    }
-                    Set<String> nextSet = nextStep(value, visited);
-                    nextSet.forEach(queue::add);
+                    list.add(value);
                 }
+
+                // maybe repeat
+                Set<String> nexts = new HashSet<>();
+                for (String value: list) {
+                    nexts.addAll(nextStep(value, visited, deadendSet));
+                }
+                queue.addAll(nexts);
                 step++;
             }
             return -1;
         }
 
-        private Set<String> nextStep(String value, Set<String> visited) {
-            Set<String> nextSet = new HashSet<>();
+        private Set<String> nextStep(String value, Set<String> visited, Set<String> deadendSet) {
+            Set<String> nexts = new HashSet<>();
 
             char[] charArray = value.toCharArray();
             for (int i = 0; i < charArray.length; i++) {
@@ -57,19 +60,18 @@ public class OpenLock {
 
                 charArray[i] = c == '9' ? '0' : (char) (c + 1);
                 String s = String.valueOf(charArray);
-                if (!visited.contains(s)) {
-                    nextSet.add(s);
+                if (!visited.contains(s) && !deadendSet.contains(s)) {
+                    nexts.add(s);
                 }
-                charArray[i] = c;
 
                 charArray[i] = c == '0' ? '9' : (char) (c - 1);
                 s = String.valueOf(charArray);
-                if (!visited.contains(s)) {
-                    nextSet.add(s);
+                if (!visited.contains(s) && !deadendSet.contains(s)) {
+                    nexts.add(s);
                 }
                 charArray[i] = c;
             }
-            return nextSet;
+            return nexts;
         }
     }
 
